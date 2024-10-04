145f0cd2d3173a97a837a0cff703fc4aa868ab18
SamTheil <137443849+SamTheil@users.noreply.github.com>
2024-10-04 17:30:45 -0400

    test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 from picamera2 import Picamera2
 from flask import Flask, send_file, request
 from io import BytesIO
